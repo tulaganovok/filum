8d5c8513699880c201cbccec8085814f1b93fb16
Otabek <tulaganovok04@gmail.com>
2026-06-11 21:06:42 +0500

    implementing searching and filtering

--- src/routes/(dashboard)/files/$fileId/index.tsx
+++ src/routes/(dashboard)/files/$fileId/index.tsx
@@ -2,10 +2,12 @@ import { Button } from '#/components/ui/button'
 import DetailedFile from '#/features/dashboard/components/detailed-file'
 import { getUserFileByIdFn } from '#/features/dashboard/utils/functions'
 import { createFileRoute, Link, useLoaderData } from '@tanstack/react-router'
-import { ArrowLeft } from 'lucide-react'
+import { ArrowLeft, Loader2 } from 'lucide-react'
 
 export const Route = createFileRoute('/(dashboard)/files/$fileId/')({
   component: RouteComponent,
+  pendingMs: 0,
+  pendingComponent: PendingComponent,
   loader: ({ params }) => getUserFileByIdFn({ data: { id: params.fileId } }),
 })
 
@@ -24,6 +26,20 @@ function RouteComponent() {
 
       <div className='max-w-xl mx-auto my-8'>
         <DetailedFile storedFile={storedFile} />
+      </div>
+    </div>
+  )
+}
+
+function PendingComponent() {
+  return (
+    <div>
+      <div className='h-72 max-w-xl bg-secondary rounded-2xl flex items-center mx-auto mt-24 justify-center'>
+        <div className='flex flex-col items-center'>
+          <Loader2 className='size-16 animate-spin text-muted-foreground' />
+          <h3 className='text-lg font-semibold text-muted-foreground mt-4'>Loading file...</h3>
+          <span className='text-sm text-muted-foreground mt-1'>This will take a few seconds.</span>
+        </div>
       </div>
     </div>
   )
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -20,7 +20,7 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
         content: 'width=device-width, initial-scale=1',
       },
       {
-        title: 'TanStack Start Starter',
+        title: 'Filum',
       },
     ],
     links: [
